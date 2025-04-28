46607935159bec04e0855c05dfb008407ac58ec7
Logan Lusher <llusher@usc.edu>
2025-04-28 06:59:54 +0000

    s git push --set-upstream origin main

--- schedwork.cpp
+++ schedwork.cpp
@@ -38,8 +38,55 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+     size_t numWorker=avail[0].size();
+     size_t numDay=avail.size();
+     vector<int> worked(numWorker, 0);
+    //initializes a schedule
+     sched=DailySchedule(numDay, vector<Worker_T>());
+     return canWork(avail, dailyNeed, maxShifts, sched, 0, 0, worked);
+
 
 
 
 }
+bool canWork(const AvailabilityMatrix& avail, const size_t maxShifts, DailySchedule& sched, size_t d, vector<int>& worked, size_t worker){
+//checks whether or not the worker is available on day d
+  if(!avail[d][worker]) return false;
 
+//check whether or not the worker has worked the max amount
+  if(worked[worker]>=maxShifts) return false;
+
+// check if the worker is already scheduled for a shift that day 
+  if(find(sched[d].begin(), sched[d].end(), worker)!=sched[d].end()) return false;
+
+  return true;
+}
+
+
+
+bool scheduler(const AvailabilityMatrix& avail, size_t dailyNeed, const size_t maxShifts, DailySchedule& sched, size_t d, size_t s, vector<int>& worked){
+  //returns true if all days have been filled
+  if(avail.size()==d)
+    return true;
+
+  size_t numWorker=avail[0].size();
+  for(size_t w=0; w<numWorker; w++){
+    if(canWork(avail, maxShifts, sched, d, worked, w)){
+      sched[d].push_back(w);
+      worked[w]++;
+
+      if(sched[d].size()==dailyNeed){
+        //recursive call to schedule the next days workflow
+        if(sheduler(avail, dailyNeed, maxShift, sched, d+1, 0, worked)) return true;
+      }
+      else{
+        //recursive call to schedule the next shift of the day
+        if(sheduler(avail, dailyNeed, maxShift, sched, d, s+1, worked)) return true;
+      }
+      //backtracking if the scheduling doesnt work and prior info needs to change
+      sched[d].pop_back();
+      worked[w]--;
+    }
+  }
+  return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+int numDashes(string str, int index);
+void wordsPsbl(const set<string>& dict, set<string>& words, string grn, string ylw, int index);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,59 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> posWords;
+    wordsPsbl(dict, posWords, in, floating, 0);
+    return posWords;
 }
 
 // Define any helper functions here
+
+// finds the number of dashes
+int numDashes(string str, int index){ 
+    int count=0;
+    for(size_t i=index; i<str.length(); i++){
+        if(str[i]=='-'){
+            count++;
+        }
+    }
+    return count;
+}
+// finds all possible word combos from the green and yellow letters given
+void wordsPsbl(const set<string>& dict, set<string>& words, string grn, string ylw, int index){
+    size_t dashNum = numDashes(grn, index);
+
+// if there are more yellow letters than dashes, no words are possible
+    if(ylw.length()>dashNum)
+    return;
+
+// if the word is completely filled out and part of the dictionary, add it to the words set
+    if(dashNum==0){
+        if(dict.find(grn)!=dict.end() && ylw.length()==0){
+            words.insert(grn);
+        }
+        return;
+    }
+/* if the correct letter hasn't been found -> it's a dash, find all the words made using the yellow, "floating", letters recursively 
+*/ 
+    if(grn[index]=='-'){
+        for(size_t i=0; i<ylw.length(); i++){
+            char l=ylw[i];
+            string newG=grn;
+            string newY=ylw;
+            newG[index]=l;
+            newY.erase(i,1);
+            wordsPsbl(dict, words, newG, newY, index+1);
+        }
+        if(ylw.length()< dashNum){
+            for(char letter='a'; letter<='z'; letter++){
+                string newG=grn;
+                newG[index]=letter;
+                wordsPsbl(dict, words, newG, ylw, index+1);
+            }
+        }
+    }
+    //if all letters are spots are filled in
+    else{
+        wordsPsbl(dict, words, grn, ylw, index+1);
+    }
+}
